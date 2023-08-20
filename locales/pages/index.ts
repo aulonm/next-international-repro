40440aaf59b06b68b44288dfb06d11e404a11d6e
Aulon Mujaj <aulon.mujaj@gmail.com>
2023-08-20 18:19:42 +0200

    more repro

--- locales/pages/index.ts
+++ locales/pages/index.ts
@@ -1,7 +1,13 @@
-import { createI18n } from 'next-international';
+import { createI18n } from "next-international";
 
-export const { useI18n, useScopedI18n, I18nProvider, useChangeLocale, defineLocale, getLocaleProps, useCurrentLocale } =
-  createI18n({
-    en: () => import('./en'),
-    fr: () => import('./fr'),
-  });
+export const {
+  useI18n,
+  useScopedI18n,
+  I18nProvider,
+  useChangeLocale,
+  defineLocale,
+  getLocaleProps,
+  useCurrentLocale,
+} = createI18n({
+  en: () => import("./en"),
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,30 +4,78 @@ import { useI18n, useScopedI18n } from "../locales/pages";
 
 export default function Page() {
   const t = useI18n();
-  const scopedT = useScopedI18n("hello");
-  const [state, setState] = useState("");
-
-  const changeState = useCallback(() => {
-    setState("loading");
-    console.log("state is: ", state);
-    console.log(scopedT("world"));
-  }, [state, scopedT]);
-
-  useEffect(() => {
-    console.log(scopedT("world.welcome", { name: "hei" }));
-    setState("dsds");
-  }, [scopedT, setState]);
+  const t2 = useScopedI18n("scope.more");
 
   return (
     <div>
-      <p>{t("hello")}</p>
-
-      {/* Both are equivalent: */}
-      <p>{t("hello.world")}</p>
-      <p>{scopedT("world")}</p>
-
-      <p>{t("welcome", { name: "John" })}</p>
-      <p>{t("welcome", { name: <strong>John</strong> })}</p>
+      <h1>CSR</h1>
+      <p></p>
+      <p>Hello: {t("hello")}</p>
+      <p>
+        Hello:{" "}
+        {t("welcome", {
+          name: "John",
+        })}
+      </p>
+      <p>
+        Hello (with React components):{" "}
+        {t("welcome", {
+          name: <strong>John</strong>,
+        })}
+      </p>
+      <p>
+        Hello:{" "}
+        {t("about.you", {
+          age: "23",
+          name: "Doe",
+        })}
+      </p>
+      <p>
+        Hello (with React components):{" "}
+        {t("about.you", {
+          age: <strong>23</strong>,
+          name: "Doe",
+        })}
+      </p>
+      <p>{t2("test")}</p>
+      <p>
+        {t2("param", {
+          param: "test",
+        })}
+      </p>
+      <p>
+        {t2("param", {
+          param: <strong>test</strong>,
+        })}
+      </p>
+      <p>{t2("and.more.test")}</p>
+      <p>{t("missing.translation.in.fr")}</p>
+      <p>
+        {t("cows", {
+          count: 1,
+        })}
+      </p>
+      <p>
+        {t("cows", {
+          count: 2,
+        })}
+      </p>
+      <p>
+        {t2("stars", {
+          count: 1,
+        })}
+      </p>
+      <p>
+        {t2("stars", {
+          count: 2,
+        })}
+      </p>
+      <button type="button" onClick={() => changeLocale("en")}>
+        EN
+      </button>
+      <button type="button" onClick={() => changeLocale("fr")}>
+        FR
+      </button>
     </div>
   );
 }

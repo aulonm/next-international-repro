86020cb30fc71add1d049162e21caac0b95b8bc1
Aulon Mujaj <aulon.mujaj@gmail.com>
2023-08-20 18:17:08 +0200

    repro locale not found

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { createI18nMiddleware } from "next-international/middleware";
+import { NextRequest } from "next/server";
+
+const I18nMiddleware = createI18nMiddleware(["en"] as const, "en", {
+  urlMappingStrategy: "rewrite",
+});
+
+export function middleware(request: NextRequest) {
+  return I18nMiddleware(request);
+}
+
+export const config = {
+  matcher: ["/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)"],
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { I18nProvider } from "@/locales/pages";
+import en from "@/locales/pages/en";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <I18nProvider locale="en" fallbackLocale={en}>
+      <Component {...pageProps} />
+    </I18nProvider>
+  );
 }
